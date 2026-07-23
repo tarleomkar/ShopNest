2267d785cbdf909735c1110be28e5132d56d5401
tarleomkar <tarleomka8@gmail.com>
2026-07-23 19:43:47 +0530

    feat: admin middleware for admin access only

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,12 +8,12 @@ import {
     getUsers,
 } from "../controllers/authController.js";
 import { admin } from "../middleware/adminMiddleware.js";
-import { protect } from "../middleware/authMiddleware.js";
+const { protect } = require("../middleware/authMiddleware.js");
 
 router.post("/register", registerUser);
 router.post("/verify-otp", verifyOtp);
 router.post("/resend-otp", resendOtp);
 router.post("/login", loginUser);
-router.get("/users", protect, admin, getUsers);
+router.get("/users", protect, admin, getUsers); // Calling middleware functions here
 
 export default router;
