9813c0fbe840d7a100c8d2132c7aa1ca972fd16f
tarleomkar <tarleomka8@gmail.com>
2026-07-27 19:45:12 +0530

    feat: all product routes implemented

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -14,6 +14,9 @@ const upload = multer({ dest: "uploads/" });
 const router = express.Router();
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 8e53bd5 (feat: all product routes implemented)
 router.route("/")
     .get(getProducts)
     .post(protect,
@@ -30,9 +33,12 @@ router.route("/:id")
         updateProduct
     )
     .delete(protect, admin, deleteProduct);
+<<<<<<< HEAD
 =======
 router.route("/").get(getProducts).post(protect, admin, upload.single("image"), createProduct);
 router.route("/:id").get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
 >>>>>>> eb7dadf (feat: product code for post and get with cloudinary setup)
+=======
+>>>>>>> 8e53bd5 (feat: all product routes implemented)
 
 export default router;
