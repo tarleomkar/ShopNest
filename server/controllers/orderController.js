7a5faa32ae5004861d881678ebe668f816e68cfe
tarleomkar <tarleomkar8@gmail.com>
2026-07-28 23:18:45 +0530

    feat: myOrders and getOrders

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -22,11 +22,33 @@ const createOrder = async (req, res) => {
             res.status(201).json({ message: 'Order created successfully', order });
         }
     } catch (error) {
-        console.error;
+        console.error(error);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const myorders = async (req, res) => {
+    try {
+        const orders = await Order.find({ user: req.user._id }).populate('items.productId', 'name price');
+        res.status(200).json(orders);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const getOrders = async (req, res) => {
+    try {
+        const orders = await Order.find({}).populate('userId', 'id name');
+        res.status(200).json(orders);
+    } catch (error) {
+        console.error(error);
         res.status(500).json({ message: error.message });
     }
 };
 
 export {
     createOrder,
+    myorders,
+    getOrders
 }
